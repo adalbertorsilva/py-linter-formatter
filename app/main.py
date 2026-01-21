4e82b9a360ba81623b20eab7144a832a8be733c8
Adalberto Silva <adalbertosilva@192.168.100.20>
2026-01-20 21:41:49 -0300

    update codestyle

--- app/main.py
+++ app/main.py
@@ -12,9 +12,14 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
     return {
         "status": "failed",
         "path": file_path,
-        "errors": [ format_linter_error(error) for error in errors ]
+        "errors": [format_linter_error(error) for error in errors]
     }
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [ format_single_linter_file(file_path, errors) if errors else { "status": "passed", "path": file_path, "errors": [] } for file_path, errors in linter_report.items() ]
+    return [
+        format_single_linter_file(file_path, errors)
+        if errors else {"status": "passed", "path": file_path, "errors": []}
+        for file_path, errors
+        in linter_report.items()
+    ]
